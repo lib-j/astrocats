4266bc7da580ff36cb0d36ace31f942531b309b7
lzkelley <lkelley@cfa.harvard.edu>
2016-08-01 17:09:19 -0400

    MAINT: update variable names in 'Paths'.

--- astrocats/catalog/paths.py
+++ astrocats/catalog/paths.py
@@ -5,9 +5,7 @@ import os
 import sys
 
 from astrocats import main, _ROOT_BASE_PATH
-from astrocats.catalog.utils import (compress_gz, is_integer, pbar,
-                                     read_json_dict, repo_priority,
-                                     uncompress_gz, uniq_cdl)
+from astrocats.catalog.utils import read_json_dict, repo_priority
 
 
 class Paths:
@@ -19,17 +17,16 @@ class Paths:
 
     Attributes
     ----------
-    catalog : `astrocats.catalog.catalog.Catalog` (sub)class object
     catalog_dir : str
     tasks_dir : str
-    PATH_BASE : str
-    PATH_INPUT : str
-    PATH_OUTPUT : str
-    REPOS_LIST : str
-    TASK_LIST : str
+    catalog_dir : str
+    input : str
+    output : str
+    repos_list_fname : str
+    task_list_fname : str
     repos_dict : dict
         Dictionary of 'repo-types: repo-lists' key-value pairs.
-        Loaded from `REPOS_LIST` file.
+        Loaded from `repos_list_fname` file.
 
     Methods
     -------
@@ -42,27 +39,48 @@ class Paths:
     """
 
     def __init__(self, catalog=None, log=None):
+        # New parameter for the overall root directory for astrocats as a whole
+        self.root = str(_ROOT_BASE_PATH)
+
+        this_file = None
         if catalog is not None:
-            this_file = sys.modules[catalog.__module__].__file__
-            log = catalog.log
+            try:
+                this_file = sys.modules[catalog.__module__].__file__
+            except AttributeError:
+                try_dirs = [os.path.join(self.root, 'astrocats'), self.root, '']
+                for td in try_dirs:
+                    this_file = os.path.join(td, catalog, '')
+                    if os.path.isdir(this_file):
+                        break
+
+                    import warnings
+                    warnings.warn("Couldnt find '{}' in '{}'...".format(catalog, td))
+                    this_file = None
+            else:
+                log = catalog.log
+
         else:
             this_file = sys.modules[self.__module__].__file__
 
+        if this_file is None:
+            raise RuntimeError("`this_file` is None!  catalog = '{}'.".format(catalog))
+
+        # If no log is given (and no catalog), construct a new one
         if log is None:
             log = main.load_log()
 
         self.log = log
+        # Catalog Directories
         self.catalog_dir = os.path.dirname(this_file)
+        self.catalog_parent = os.path.abspath(os.path.join(self.catalog_dir, os.pardir))
         self.tasks_dir = os.path.join(self.catalog_dir, 'tasks')
-        # New parameter for the overall root directory for astrocats as a whole
-        self.ROOT = str(_ROOT_BASE_PATH)
-        self.PATH_BASE = str(self.catalog_dir)
-        self.PATH_INPUT = os.path.join(self.PATH_BASE, 'input', '')
-        self.PATH_OUTPUT = os.path.join(self.PATH_BASE, 'output', '')
-        # critical datafiles
-        self.REPOS_LIST = os.path.join(self.PATH_INPUT, 'repos.json')
-        self.TASK_LIST = os.path.join(self.PATH_INPUT, 'tasks.json')
-        self.repos_dict = read_json_dict(self.REPOS_LIST)
+        self.input = os.path.join(self.catalog_dir, 'input', '')
+        self.output = os.path.join(self.catalog_dir, 'output', '')
+
+        # Catalog data files
+        self.repos_list_fname = os.path.join(self.input, 'repos.json')
+        self.task_list_fname = os.path.join(self.input, 'tasks.json')
+        self.repos_dict = read_json_dict(self.repos_list_fname)
         return
 
     def _get_repo_file_list(self, repo_folders, normal=True, bones=True):
@@ -95,7 +113,7 @@ class Paths:
             bone_path = bone_path[0]
         except TypeError:
             pass
-        bone_path = os.path.join(self.PATH_OUTPUT, bone_path, '')
+        bone_path = os.path.join(self.output, bone_path, '')
         return bone_path
 
     def get_repo_input_folders(self):
@@ -105,7 +123,7 @@ class Paths:
         repo_folders += self.repos_dict['external']
         repo_folders += self.repos_dict['internal']
         repo_folders = list(sorted(set(repo_folders)))
-        repo_folders = [os.path.join(self.PATH_INPUT, rf)
+        repo_folders = [os.path.join(self.input, rf)
                         for rf in repo_folders if len(rf)]
         return repo_folders
 
@@ -127,6 +145,6 @@ class Paths:
             repo_folders += self.repos_dict['boneyard']
         repo_folders = list(sorted(list(set(repo_folders)),
                                    key=lambda key: repo_priority(key)))
-        repo_folders = [os.path.join(self.PATH_OUTPUT, rf)
+        repo_folders = [os.path.join(self.output, rf)
                         for rf in repo_folders if len(rf)]
         return repo_folders
