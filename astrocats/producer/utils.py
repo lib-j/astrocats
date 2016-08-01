ce1b3442150cd41c2473f36aeb23559de5ac4179
lzkelley <lkelley@cfa.harvard.edu>
2016-08-01 09:23:01 -0400

    MAINT: remove some unused functions.

--- astrocats/producer/utils.py
+++ astrocats/producer/utils.py
@@ -4,34 +4,15 @@ import os
 import requests
 
 
-def event_filename(name):
-    return (name.replace('/', '_'))
-
-
 def touch(fname, times=None):
     with open(fname, 'a'):
         os.utime(fname, times)
-
-
-def utf8(x):
-    return str(x, 'utf-8')
 
 
 def label_format(label):
     newlabel = label.replace('Angstrom', 'Å')
     newlabel = newlabel.replace('^2', '²')
     return newlabel
-
-
-def is_valid_link(url):
-    response = requests.get(url)
-    try:
-        response.raise_for_status()
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        return False
-    return True
 
 
 def get_first_value(catalog, name, field):
