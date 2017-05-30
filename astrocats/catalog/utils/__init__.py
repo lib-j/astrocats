72cc0cf659e73b5d1d0e6b644a29cc2b418ca3ea
lzkelley <lkelley@cfa.harvard.edu>
2017-05-30 16:07:19 -0400

    ENH: add new 'utils.math' submodule with methods for rounding and converting errors to log space

--- astrocats/catalog/utils/__init__.py
+++ astrocats/catalog/utils/__init__.py
@@ -1,8 +1,8 @@
 """General utility functions used by multiple OSC scripts.
 """
+# flake8: noqa  --- ignore imported but unused flake8 warnings
 
-from . import (dates, digits, imports, logger, plotting, sorting, strings,
-               tq_funcs)
+from . import (dates, digits, imports, logger, plotting, sorting, strings, tq_funcs)
 from .dates import *
 from .digits import *
 from .imports import *
@@ -12,6 +12,7 @@ from .plotting import *
 from .sorting import *
 from .strings import *
 from .tq_funcs import *
+from .math import *
 
 __all__ = []
 __all__.extend(dates.__all__)
@@ -23,3 +24,4 @@ __all__.extend(plotting.__all__)
 __all__.extend(sorting.__all__)
 __all__.extend(strings.__all__)
 __all__.extend(tq_funcs.__all__)
+__all__.extend(math.__all__)
